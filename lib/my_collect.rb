d0609576dbcf9af6d0ee5f01b098b631b345292b
Ty Raino <mstymetris@yahoo.com>
2020-03-13 01:01:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,13 @@
 def my_collect(collection)
   i = 0
+  new_collection = []
   while i < collection.length
+  new_collection << yield(collection[i])
     i += 1
 end
+new_collection
+end
+
+my_collect(collection) do |lang|
+  lang.upcase
 end
